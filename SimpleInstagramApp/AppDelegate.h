72d1a5add1be4694b073e65d3f101bcfc68f59cc
danilNil <danil.nikiphorov@gmail.com>
2012-10-20 13:13:26 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "first commit""""""""
    This reverts commit fece09111a5547a7a63c5a45e244ea8445a2f76f.

---
+++ SimpleInstagramApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleInstagramApp
+//
+//  Created by Danil on 22.09.12.
+//  Copyright (c) 2012 Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
