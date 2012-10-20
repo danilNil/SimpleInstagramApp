10af4ea3070239aa79d6cc6130a705d0c4a39572
danilNil <danil.nikiphorov@gmail.com>
2012-10-20 13:13:50 +0700

    commit

---
+++ SimpleInstagramApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleInstagramApp
+//
+//  Created by Danil on 22.09.12.
+//  Copyright (c) 2012 Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
